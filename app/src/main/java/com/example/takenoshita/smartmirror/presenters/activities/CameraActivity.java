88dbee4e522aefa26b2043fd4b87d6c37ac6d414
RyoBamboo <itakedaka@gmail.com>
2016-12-04 11:49:30 +0900

    CameraActivityの作成

---
+++ app/src/main/java/com/example/takenoshita/smartmirror/presenters/activities/CameraActivity.java
@@ -0,0 +1,12 @@
+package com.example.takenoshita.smartmirror.presenters.activities;
+
+import android.os.Bundle;
+
+public class CameraActivity extends BaseActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
